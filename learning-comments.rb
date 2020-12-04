ed2e51c0527f0b595eecc66aa1ae40ad3e73128f
nick repasy <nrepasy@gmail.com>
2020-12-04 18:46:10 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+name = "byron"
+# name = "Luca"
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
+#=> We're sorry, but per helth inspector's rules, Luca is not allowed in the stor.
